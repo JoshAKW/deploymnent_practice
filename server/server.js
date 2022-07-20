3da969c6f6e387dab549f1bae4e76bdead7ddc2a
Joshua West <JoshuaHWest@gmail.com>
2022-07-20 01:21:55 -0600

    debbuging

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
-app,use(cors());
+app.use(cors());
 app.use(express.json());
 
 app.get("/", function(req,res) {
